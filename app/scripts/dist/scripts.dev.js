49baf7092d09dcbae77354e15e9851f7830df033
Jake Miller <jake83miller@gmail.com>
2025-08-20 16:01:05 -0400

    im trucking through this and not paying attention to git

--- app/scripts/dist/scripts.dev.js
+++ app/scripts/dist/scripts.dev.js
@@ -1,30 +1,33 @@
-"use strict";
+"use strict"; // use my location
 
-navigator.geolocation.getCurrentPosition(function _callee(position) {
-  var latitude, longitude, weatherInfo;
-  return regeneratorRuntime.async(function _callee$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          latitude = position.coords.latitude;
-          longitude = position.coords.longitude;
-          _context.next = 4;
-          return regeneratorRuntime.awrap(getWeather(latitude, longitude));
+var myLocationButton = document.querySelector(".js-my-location");
+myLocationButton.addEventListener("click", function () {
+  navigator.geolocation.getCurrentPosition(function _callee(position) {
+    var latitude, longitude, weatherInfo;
+    return regeneratorRuntime.async(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            _context.next = 4;
+            return regeneratorRuntime.awrap(getWeather(latitude, longitude));
 
-        case 4:
-          weatherInfo = _context.sent;
-          windspeed = parseFloat(weatherInfo.windSpeed.replace("mph", "").trim());
-          console.log(windspeed);
-          readings(windspeed);
+          case 4:
+            weatherInfo = _context.sent;
+            windspeed = parseFloat(weatherInfo.windSpeed.replace("mph", "").trim());
+            humidity = weatherInfo.relativeHumidity;
+            readings(windspeed, humidity);
 
-        case 8:
-        case "end":
-          return _context.stop();
+          case 8:
+          case "end":
+            return _context.stop();
+        }
       }
-    }
+    });
+  }, function (error) {// Handle permission denied and or other errors
   });
-}, function (error) {// Handle permission denied and or other errors
-});
+}); // handle general json
 
 function getJSON(url) {
   var res;
@@ -62,10 +65,11 @@ function getJSON(url) {
       }
     }
   });
-}
+} // handle api for weather information on weather.gov
+
 
 function getWeather(latitude, longitude) {
-  var pointData, hourlyUrl, forecastData, nextHour;
+  var pointData, hourlyUrl, forecastData, d, hours, periods, nextHourIndex, nextHour;
   return regeneratorRuntime.async(function getWeather$(_context3) {
     while (1) {
       switch (_context3.prev = _context3.next) {
@@ -76,28 +80,34 @@ function getWeather(latitude, longitude) {
         case 2:
           pointData = _context3.sent;
           hourlyUrl = pointData.properties.forecastHourly;
-          _context3.next = 6;
+          console.log(hourlyUrl);
+          _context3.next = 7;
           return regeneratorRuntime.awrap(getJSON(hourlyUrl));
 
-        case 6:
+        case 7:
           forecastData = _context3.sent;
-          nextHour = forecastData.properties.periods[0];
+          // get the date and hour of the user to grab the correct index of periods
+          d = new Date();
+          hours = d.getHours();
+          periods = forecastData.properties.periods;
+          nextHourIndex = periods.findIndex(function (period) {
+            var periodHour = new Date(period.startTime).getHours();
+            return periodHour === hours;
+          });
+          nextHour = periods[nextHourIndex];
           return _context3.abrupt("return", {
             windSpeed: nextHour.windSpeed,
-            // e.g. "8 mph"
-            relativeHumidity: nextHour.relativeHumidity.value || null
+            relativeHumidity: nextHour.relativeHumidity.value
           });
 
-        case 9:
+        case 14:
         case "end":
           return _context3.stop();
       }
     }
   });
-}
+} // functions to handle displaying needed information in graphs
 
-var windspeedCircle = document.querySelector(".js-graph-circle--windspeed");
-var windspeedText = document.querySelector(".js-windspeed-text");
 
 var getOffset = function getOffset() {
   var val = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
@@ -108,11 +118,18 @@ var getOffset = function getOffset() {
 
 var readings = function readings() {
   var windspeed = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-  var humidity = arguments.length > 1 ? arguments[1] : undefined;
+  var humidity = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  var windspeedCircle = document.querySelector(".js-graph-circle--windspeed");
+  var windspeedText = document.querySelector(".js-windspeed-text");
+  var humidityCircle = document.querySelector(".js-graph-circle--humidity");
+  var humidityText = document.querySelector(".js-humidity-text");
   var windspeedPercentage = Number(windspeed / 10 * 100);
   windspeedCircle.style.strokeDashoffset = getOffset(windspeedPercentage);
   windspeedText.textContent = "".concat(Number(windspeed), " mph");
-  windspeedPercentage <= 79 ? windspeedCircle.style.stroke = "#cc9c50" : windspeedPercentage >= 100 ? windspeedCircle.style.stroke = "red" : windspeedCircle.style.stroke = "#f87819";
+  humidityCircle.style.strokeDashoffset = getOffset(humidity);
+  humidityText.textContent = "".concat(Number(humidity), "%");
+  windspeedPercentage <= 79 ? windspeedCircle.style.stroke = "#cc9c50" : windspeedPercentage >= 100 ? windspeedCircle.style.stroke = "#c94c26" : windspeedCircle.style.stroke = "#f87819";
+  humidity > 40 ? humidityCircle.style.stroke = "#cc9c50" : humidity <= 30 ? humidityCircle.style.stroke = "#c94c26" : humidityCircle.style.stroke = "#f87819";
 };
 
 document.addEventListener("DOMContentLoaded", function () {
--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,60 +1,89 @@
 "use strict";
+// use my location
+const myLocationButton = document.querySelector(".js-my-location");
 
-navigator.geolocation.getCurrentPosition(
-  async (position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const weatherInfo = await getWeather(latitude, longitude);
-    windspeed = parseFloat(weatherInfo.windSpeed.replace("mph", "").trim());
-    console.log(windspeed);
-    readings(windspeed);
-  },
-  (error) => {
-    // Handle permission denied and or other errors
-  }
-);
+myLocationButton.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(
+    async (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const weatherInfo = await getWeather(latitude, longitude);
+      windspeed = parseFloat(weatherInfo.windSpeed.replace("mph", "").trim());
+      humidity = weatherInfo.relativeHumidity;
+      readings(windspeed, humidity);
+    },
+    (error) => {
+      // Handle permission denied and or other errors
+    }
+  );
+});
 
+// handle general json
 async function getJSON(url) {
   const res = await fetch(url, { headers: { "User-Agent": "SafeBurnApp" } });
   if (!res.ok) throw new Error(`Request failed: ${res.status}`);
   return await res.json();
 }
 
+// handle api for weather information on weather.gov
 async function getWeather(latitude, longitude) {
   // Get grid endpoint for the location
   const pointData = await getJSON(
     `https://api.weather.gov/points/${latitude},${longitude}`
   );
   const hourlyUrl = pointData.properties.forecastHourly;
-
+  console.log(hourlyUrl);
   const forecastData = await getJSON(hourlyUrl);
-  const nextHour = forecastData.properties.periods[0];
+  // get the date and hour of the user to grab the correct index of periods
+  const d = new Date();
+  let hours = d.getHours();
+
+  const periods = forecastData.properties.periods;
+  const nextHourIndex = periods.findIndex((period) => {
+    const periodHour = new Date(period.startTime).getHours();
+    return periodHour === hours;
+  });
+
+  const nextHour = periods[nextHourIndex];
 
   return {
-    windSpeed: nextHour.windSpeed, // e.g. "8 mph"
-    relativeHumidity: nextHour.relativeHumidity.value || null,
+    windSpeed: nextHour.windSpeed,
+    relativeHumidity: nextHour.relativeHumidity.value,
   };
 }
 
-const windspeedCircle = document.querySelector(".js-graph-circle--windspeed");
-const windspeedText = document.querySelector(".js-windspeed-text");
-
+// functions to handle displaying needed information in graphs
 const getOffset = (val = 0) => {
   const offset = 1100 - Math.round((860 / 100) * val);
   const cleanOffset = Number(offset >= 240 ? offset.toFixed(2) : 240);
   return cleanOffset;
 };
 
-const readings = (windspeed = 0, humidity) => {
+const readings = (windspeed = 0, humidity = 0) => {
+  const windspeedCircle = document.querySelector(".js-graph-circle--windspeed");
+  const windspeedText = document.querySelector(".js-windspeed-text");
+  const humidityCircle = document.querySelector(".js-graph-circle--humidity");
+  const humidityText = document.querySelector(".js-humidity-text");
+
   const windspeedPercentage = Number((windspeed / 10) * 100);
+
   windspeedCircle.style.strokeDashoffset = getOffset(windspeedPercentage);
   windspeedText.textContent = `${Number(windspeed)} mph`;
+
+  humidityCircle.style.strokeDashoffset = getOffset(humidity);
+  humidityText.textContent = `${Number(humidity)}%`;
 
   windspeedPercentage <= 79
     ? (windspeedCircle.style.stroke = "#cc9c50")
     : windspeedPercentage >= 100
-    ? (windspeedCircle.style.stroke = "red")
+    ? (windspeedCircle.style.stroke = "#c94c26")
     : (windspeedCircle.style.stroke = "#f87819");
+
+  humidity > 40
+    ? (humidityCircle.style.stroke = "#cc9c50")
+    : humidity <= 30
+    ? (humidityCircle.style.stroke = "#c94c26")
+    : (humidityCircle.style.stroke = "#f87819");
 };
 
 document.addEventListener("DOMContentLoaded", () => setTimeout(readings, 100));
